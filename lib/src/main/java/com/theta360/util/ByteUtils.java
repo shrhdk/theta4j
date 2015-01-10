b64a63f828258231c73b4f52a1b9ebfd15227c87
Hideki Shiro <hideki@shiro.be>
2015-01-10 22:25:08 +0900

    Refactor ByteUtils

--- lib/src/main/java/com/theta360/util/ByteUtils.java
+++ lib/src/main/java/com/theta360/util/ByteUtils.java
@@ -1,28 +1,29 @@
 package com.theta360.util;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
 public final class ByteUtils {
     private ByteUtils() {
     }
 
     public static byte[] join(byte[]... byteArrays) {
+        // Calculate Total Length
         int length = 0;
         for (byte[] bytes : byteArrays) {
             length += bytes.length;
         }
 
-        byte[] joined = new byte[length];
-
-        // join
-        int pos = 0;
-        for (byte[] src : byteArrays) {
-            if (src == null) {
-                throw new NullPointerException();
+        // Join Byte Arrays
+        ByteArrayOutputStream baos = new ByteArrayOutputStream(length);
+        for (byte[] byteArray : byteArrays) {
+            try {
+                baos.write(byteArray);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
-
-            System.arraycopy(src, 0, joined, pos, src.length);
-            pos += src.length;
         }
 
-        return joined;
+        return baos.toByteArray();
     }
 }
