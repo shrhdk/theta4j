bb904f06b226690892ce8842e595489190533ca0
Hideki Shiro <hideki@shiro.be>
2015-01-12 15:44:27 +0900

    Implement PacketOutputStream#writeData(byte[])

--- lib/src/test/java/com/theta360/ptp/io/PacketOutputStreamTest.java
+++ lib/src/test/java/com/theta360/ptp/io/PacketOutputStreamTest.java
@@ -1,7 +1,13 @@
 package com.theta360.ptp.io;
 
+import com.theta360.ptp.data.TransactionID;
+import com.theta360.ptp.packet.EndDataPacket;
 import com.theta360.ptp.packet.PtpIpPacket;
+import com.theta360.ptp.packet.StartDataPacket;
+import com.theta360.ptp.type.UINT32;
+import com.theta360.ptp.type.UINT64;
 import com.theta360.test.categories.UnitTest;
+import com.theta360.util.ByteUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -9,6 +15,7 @@ import org.junit.experimental.categories.Category;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.math.BigInteger;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
@@ -43,6 +50,25 @@ public class PacketOutputStreamTest {
 
         // act
         pos.write(new PtpIpPacket(PtpIpPacket.Type.INIT_COMMAND_REQUEST, given));
+
+        // verify
+        byte[] actual = baos.toByteArray();
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void writeData() throws IOException {
+        // given
+        byte[] given = new byte[]{0x12, 0x34};
+
+        // expected
+        byte[] expected = ByteUtils.join(
+                new StartDataPacket(new UINT32(1), new UINT64(BigInteger.valueOf(given.length))).bytes(),
+                new EndDataPacket(new UINT32(2), given).bytes()
+        );
+
+        // act
+        pos.writeData(new TransactionID(), given);
 
         // verify
         byte[] actual = baos.toByteArray();
