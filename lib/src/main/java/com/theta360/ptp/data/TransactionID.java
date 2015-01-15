aa0867f6e27f20a8cccda99fa394135cc9e7666f
Hideki Shiro <hideki@shiro.be>
2015-01-16 04:12:41 +0900

    Fix TransactionID Add TransactionIDTest

--- lib/src/main/java/com/theta360/ptp/data/TransactionID.java
+++ lib/src/main/java/com/theta360/ptp/data/TransactionID.java
@@ -8,7 +8,7 @@ import java.util.Iterator;
  * Iterator of Transaction ID for PTP-IP Packet
  */
 public final class TransactionID implements Iterator<UINT32> {
-    private UINT32 current = new UINT32(0);
+    private long current = 0;
 
     /**
      * Always returns true.
@@ -20,17 +20,15 @@ public final class TransactionID implements Iterator<UINT32> {
 
     /**
      * Returns the following values from initial state.
-     * 1, 2, ..., [Max value of UINT32], 1, 2, ...
+     * 0, 1, 2, ..., 0xFFFFFFFE, 0, 1, 2, ...
      */
     @Override
     public UINT32 next() {
-        if (current.equals(UINT32.MAX_VALUE)) {
-            current = new UINT32(1);
-        } else {
-            current = new UINT32(current.longValue() + 1);
+        if (0xFFFF_FFFFL <= current) {
+            current = 0;
         }
 
-        return current;
+        return new UINT32(current++);
     }
 
     /**
