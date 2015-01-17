2e708b7b358ce642d9babf4ee2db28df8a9796bb
Hideki Shiro <hideki@shiro.be>
2015-01-17 19:37:53 +0900

    Add ThetaEventListener

--- lib/src/main/java/com/theta360/theta/ThetaEventListener.java
+++ lib/src/main/java/com/theta360/theta/ThetaEventListener.java
@@ -1,67 +1,13 @@
 package com.theta360.theta;
 
-import com.theta360.ptp.PtpEventListener;
-import com.theta360.ptp.type.UINT16;
 import com.theta360.ptp.type.UINT32;
 
-public abstract class ThetaEventListener implements PtpEventListener {
-    @Override
-    public void onCancelTransaction() {
-        // NOP
-    }
+public interface ThetaEventListener {
+    void onObjectAdded(UINT32 objectHandle);
 
-    @Override
-    public void onObjectRemoved(UINT32 objectHandle) {
-        // NOP
-    }
+    void onDevicePropChanged(UINT32 devicePropCode);
 
-    @Override
-    public void onStoreAdded(UINT32 storageID) {
-        // NOP
-    }
+    void onStoreFull(UINT32 storageID);
 
-    @Override
-    public void onStoreRemoved(UINT32 storageID) {
-        // NOP
-    }
-
-    @Override
-    public void onObjectInfoChanged(UINT32 objectHandle) {
-        // NOP
-    }
-
-    @Override
-    public void onDeviceInfoChanged() {
-        // NOP
-    }
-
-    @Override
-    public void onRequestObjectTransfer(UINT32 objectHandle) {
-        // NOP
-    }
-
-    @Override
-    public void onDeviceReset() {
-        // NOP
-    }
-
-    @Override
-    public void onStorageInfoChanged(UINT32 storageID) {
-        // NOP
-    }
-
-    @Override
-    public void onUnreportedStatus() {
-        // NOP
-    }
-
-    @Override
-    public void onVendorExtendedCode(UINT16 eventCode, UINT32 p1, UINT32 p2, UINT32 p3) {
-        // NOP
-    }
-
-    @Override
-    public void onError(Exception e) {
-        // NOP
-    }
+    void onCaptureComplete(UINT32 transactionID);
 }
