b9bc4eff52b177eadade916c9c27da7e23108f00
Hideki Shiro <hideki@shiro.be>
2015-01-25 17:50:37 +0900

    Add ThetaEventAdapter

---
+++ lib/src/main/java/com/theta360/theta/ThetaEventAdapter.java
@@ -0,0 +1,27 @@
+package com.theta360.theta;
+
+public abstract class ThetaEventAdapter implements ThetaEventListener {
+    @Override
+    public void onObjectAdded(long objectHandle) {
+    }
+
+    @Override
+    public void onCaptureStatusChanged() {
+    }
+
+    @Override
+    public void onRecordingTimeChanged() {
+    }
+
+    @Override
+    public void onRemainingRecordingTimeChanged() {
+    }
+
+    @Override
+    public void onStoreFull(long storageID) {
+    }
+
+    @Override
+    public void onCaptureComplete(long transactionID) {
+    }
+}
