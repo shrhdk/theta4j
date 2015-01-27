535eebb9201ecbad6ab66a3c2962c3607a438545
Hideki Shiro <hideki@shiro.be>
2015-01-27 21:29:28 +0900

    Add INT8 and UINT8

---
+++ lib/src/main/java/com/theta360/ptp/type/INT8.java
@@ -0,0 +1,5 @@
+package com.theta360.ptp.type;
+
+public final class INT8 {
+    public static final int SIZE = 8;
+}
---
+++ lib/src/main/java/com/theta360/ptp/type/UINT8.java
@@ -0,0 +1,5 @@
+package com.theta360.ptp.type;
+
+public final class UINT8 {
+    public static final int SIZE = 8;
+}
