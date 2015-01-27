20d16296d0e2c9bde77c8d0be52053087d37e2e8
Hideki Shiro <hideki@shiro.be>
2015-01-27 22:05:15 +0900

    Add UINT128

---
+++ lib/src/main/java/com/theta360/ptp/type/UINT128.java
@@ -0,0 +1,139 @@
+package com.theta360.ptp.type;
+
+import com.theta360.util.Validators;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.math.BigInteger;
+
+public final class UINT128 extends Number implements Comparable<UINT128> {
+    private static final BigInteger MIN_INTEGER_VALUE = BigInteger.ZERO;
+    private static final BigInteger MAX_INTEGER_VALUE = new BigInteger("00FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF", 16);
+
+    private final BigInteger bigInteger;
+    private final byte[] bytes;
+
+    // Utility Field
+
+    /**
+     * Size of type in bytes.
+     */
+    public static final int SIZE_IN_BYTES = 16;
+
+    public static final UINT128 MIN_VALUE = new UINT128(MIN_INTEGER_VALUE);
+    public static final UINT128 MAX_VALUE = new UINT128(MAX_INTEGER_VALUE);
+
+    public static final UINT128 ZERO = new UINT128(0);
+    public static final UINT128 ONE = new UINT128(1);
+    public static final UINT128 TEN = new UINT128(10);
+
+    // Constructor
+
+    public UINT128(long value) {
+        this(BigInteger.valueOf(value));
+    }
+
+    public UINT128(BigInteger value) {
+        Validators.validateNonNull("value", value);
+
+        if (value.signum() == -1) {
+            throw new IllegalArgumentException();
+        }
+
+        if (0 < value.compareTo(MAX_INTEGER_VALUE)) {
+            throw new IllegalArgumentException();
+        }
+
+        this.bytes = UINT.toLittleEndian(SIZE_IN_BYTES, value.toByteArray());
+        this.bigInteger = value;
+    }
+
+    public UINT128(byte[] bytes) {
+        Validators.validateNonNull("bytes", bytes);
+        Validators.validateLength("bytes", bytes, SIZE_IN_BYTES);
+
+        this.bytes = bytes.clone();
+
+        // UINT64 constructor arguments are little endian and are not two's complement.
+        // BigInteger constructor needs big endian two's complement value.
+        // So it need to reverse order of arguments and add 0x00 to top.
+        this.bigInteger = new BigInteger(new byte[]{
+                0x00,
+                bytes[15], bytes[14], bytes[13], bytes[12], bytes[11], bytes[10], bytes[9], bytes[8],
+                bytes[7], bytes[6], bytes[5], bytes[4], bytes[3], bytes[2], bytes[1], bytes[0]
+        });
+    }
+
+    // Static Factory Method
+
+    public static UINT128 read(InputStream is) throws IOException {
+        byte[] bytes = new byte[SIZE_IN_BYTES];
+
+        if (is.read(bytes) == -1) {
+            throw new IOException();
+        }
+
+        return new UINT128(bytes);
+    }
+
+    // Getter
+
+    public byte[] bytes() {
+        return bytes.clone();
+    }
+
+    public BigInteger bigInteger() {
+        return bigInteger;
+    }
+
+    // Number
+
+    @Override
+    public int intValue() {
+        return bigInteger.intValue();
+    }
+
+    @Override
+    public long longValue() {
+        return bigInteger.longValue();
+    }
+
+    @Override
+    public float floatValue() {
+        return bigInteger.floatValue();
+    }
+
+    @Override
+    public double doubleValue() {
+        return bigInteger.doubleValue();
+    }
+
+    // Compare
+
+    @Override
+    public int compareTo(UINT128 o) {
+        return bigInteger.compareTo(o.bigInteger);
+    }
+
+    // Basic Method
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UINT128 uint128 = (UINT128) o;
+
+        return bigInteger.equals(uint128.bigInteger);
+    }
+
+    @Override
+    public int hashCode() {
+        return bigInteger.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return bigInteger.toString();
+    }
+}
