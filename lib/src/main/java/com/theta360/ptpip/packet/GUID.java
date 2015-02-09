b7b5c5947442a0c6c4d559a5af2e48b89b7f1792
Hideki Shiro <hideki@shiro.be>
2015-02-09 23:13:28 +0900

    Add Test for GUID

--- lib/src/main/java/com/theta360/ptpip/packet/GUID.java
+++ lib/src/main/java/com/theta360/ptpip/packet/GUID.java
@@ -1,8 +1,8 @@
 package com.theta360.ptpip.packet;
 
-import com.theta360.ptp.io.PtpInputStream;
-
+import java.io.EOFException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.util.UUID;
@@ -11,6 +11,7 @@ final class GUID {
     public static final int SIZE_IN_BYTES = 16;
 
     private GUID() {
+        throw new AssertionError();
     }
 
     public static byte[] toBytes(UUID guid) {
@@ -21,20 +22,16 @@ final class GUID {
         return buffer.array();
     }
 
-    public static UUID toUUID(byte[] bytes) {
+    public static UUID read(InputStream is) throws IOException {
+        byte[] bytes = new byte[SIZE_IN_BYTES];
+
+        if (is.read(bytes) != SIZE_IN_BYTES) {
+            throw new EOFException();
+        }
+
         ByteBuffer buffer = ByteBuffer.wrap(bytes);
         buffer.order(ByteOrder.LITTLE_ENDIAN);
 
         return new UUID(buffer.getLong(), buffer.getLong());
-    }
-
-    public static UUID read(PtpInputStream pis) throws IOException {
-        byte[] bytes = new byte[SIZE_IN_BYTES];
-
-        if (pis.read(bytes) == -1) {
-            throw new IOException();
-        }
-
-        return toUUID(bytes);
     }
 }
--- lib/src/test/java/com/theta360/ptpip/packet/GUIDTest.java
+++ lib/src/test/java/com/theta360/ptpip/packet/GUIDTest.java
@@ -2,22 +2,81 @@ package com.theta360.ptpip.packet;
 
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.UUID;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class GUIDTest {
+    // toBytes
+
+    @Test(expected = NullPointerException.class)
+    public void nullToBytes() {
+        // act
+        GUID.toBytes(null);
+    }
+
     @Test
-    public void encodeAndDecode() {
+    public void toBytes() {
         // given
-        UUID given = UUID.randomUUID();
+        long msb = 0x0706050403020100L;
+        long lsb = 0x0F0E0D0C0B0A0908L;
+        UUID given = new UUID(msb, lsb);
+
+        // expected
+        byte[] expected = new byte[]{
+                0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+                0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F
+        };
 
         // act
-        byte[] bytes = GUID.toBytes(given);
-        UUID actual = GUID.toUUID(bytes);
+        byte[] actual = GUID.toBytes(given);
 
         // verify
-        assertThat(actual, is(given));
+        assertThat(actual, is(expected));
+    }
+
+    // read
+
+    @Test(expected = NullPointerException.class)
+    public void readNull() throws IOException {
+        // act
+        GUID.read(null);
+    }
+
+    @Test(expected = EOFException.class)
+    public void readFromInsufficientStream() throws IOException {
+        // given
+        InputStream given = new ByteArrayInputStream(new byte[]{
+                0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+                0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E
+        });
+
+        // act
+        GUID.read(given);
+    }
+
+    @Test
+    public void read() throws IOException {
+        // given
+        InputStream given = new ByteArrayInputStream(new byte[]{
+                0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+                0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F
+        });
+
+        // expected
+        long msb = 0x0706050403020100L;
+        long lsb = 0x0F0E0D0C0B0A0908L;
+        UUID expected = new UUID(msb, lsb);
+
+        // act
+        UUID actual = GUID.read(given);
+
+        // verify
+        assertThat(actual, is(expected));
     }
 }
