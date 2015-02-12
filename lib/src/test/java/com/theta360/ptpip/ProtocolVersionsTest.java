38c61697ce92e1c2f27ccfb9c385d65f30469fad
Hideki Shiro <hideki@shiro.be>
2015-02-12 23:06:16 +0900

    Add test for ProtocolVersions

---
+++ lib/src/test/java/com/theta360/ptpip/ProtocolVersionsTest.java
@@ -0,0 +1,15 @@
+package com.theta360.ptpip;
+
+import com.theta360.TestUtils;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class ProtocolVersionsTest {
+    // Constructor
+
+    @Test
+    public void isUtilClass() throws Throwable {
+        assertTrue(TestUtils.isUtilClass(ProtocolVersions.class));
+    }
+}
