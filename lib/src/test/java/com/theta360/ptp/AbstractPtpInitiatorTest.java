7da031f65d4f9be2214956ef83d7bb510ed0a577
Hideki Shiro <hideki@shiro.be>
2015-02-12 23:42:15 +0900

    Add test for AbstractPtpInitiator

---
+++ lib/src/test/java/com/theta360/ptp/AbstractPtpInitiatorTest.java
@@ -0,0 +1,84 @@
+package com.theta360.ptp;
+
+import com.theta360.ptp.code.Code;
+import com.theta360.ptp.code.OperationCode;
+import com.theta360.ptp.type.UINT16;
+import com.theta360.ptp.type.UINT32;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.mockito.Mockito.*;
+
+public class AbstractPtpInitiatorTest {
+    private static final Code<UINT16> OPERATION_CODE = OperationCode.UNDEFINED;
+    private static final UINT32 P1 = new UINT32(1);
+    private static final UINT32 P2 = new UINT32(2);
+    private static final UINT32 P3 = new UINT32(3);
+    private static final UINT32 P4 = new UINT32(4);
+
+    private PtpInitiator sub;
+
+    @Before
+    public void setUp() {
+        sub = mock(AbstractPtpInitiator.class, CALLS_REAL_METHODS);
+    }
+
+    // sendOperation
+
+    @Test
+    public void sendOperation() throws IOException {
+        // act
+        sub.sendOperation(OPERATION_CODE);
+
+        // verify
+        verify(sub).sendOperation(
+                OPERATION_CODE,
+                UINT32.ZERO, UINT32.ZERO, UINT32.ZERO, UINT32.ZERO, UINT32.ZERO);
+    }
+
+    @Test
+    public void sendOperationWithP1() throws IOException {
+        // act
+        sub.sendOperation(OPERATION_CODE, P1);
+
+        // verify
+        verify(sub).sendOperation(
+                OPERATION_CODE,
+                P1, UINT32.ZERO, UINT32.ZERO, UINT32.ZERO, UINT32.ZERO);
+    }
+
+    @Test
+    public void sendOperationWithP12() throws IOException {
+        // act
+        sub.sendOperation(OPERATION_CODE, P1, P2);
+
+        // verify
+        verify(sub).sendOperation(
+                OPERATION_CODE,
+                P1, P2, UINT32.ZERO, UINT32.ZERO, UINT32.ZERO);
+    }
+
+    @Test
+    public void sendOperationWithP123() throws IOException {
+        // act
+        sub.sendOperation(OPERATION_CODE, P1, P2, P3);
+
+        // verify
+        verify(sub).sendOperation(
+                OPERATION_CODE,
+                P1, P2, P3, UINT32.ZERO, UINT32.ZERO);
+    }
+
+    @Test
+    public void sendOperationWithP1234() throws IOException {
+        // act
+        sub.sendOperation(OPERATION_CODE, P1, P2, P3, P4);
+
+        // verify
+        verify(sub).sendOperation(
+                OPERATION_CODE,
+                P1, P2, P3, P4, UINT32.ZERO);
+    }
+}
