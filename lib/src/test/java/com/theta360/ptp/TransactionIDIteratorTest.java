ff7260be0ebe59b35354cc013812ab7ddd70b7ea
Hideki Shiro <hideki@shiro.be>
2015-02-12 23:07:06 +0900

    Add test case of TransactionIDIterator

--- lib/src/test/java/com/theta360/ptp/TransactionIDIteratorTest.java
+++ lib/src/test/java/com/theta360/ptp/TransactionIDIteratorTest.java
@@ -8,25 +8,35 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class TransactionIDIteratorTest {
+    @Test(expected = UnsupportedOperationException.class)
+    public void removeIsNotSupported() {
+        // given
+        TransactionIDIterator given = new TransactionIDIterator();
+
+        // act
+        given.remove();
+    }
+
     @Test
     public void iterate() {
-        TransactionIDIterator transactionIDIterator = new TransactionIDIterator();
+        TransactionIDIterator iterator = new TransactionIDIterator();
 
         for (long i = 0; i < 3; i++) {
-            assertThat(transactionIDIterator.next().longValue(), is(i));
+            assertThat(iterator.hasNext(), is(true));
+            assertThat(iterator.next().longValue(), is(i));
         }
     }
 
     @Test
     public void loop() throws NoSuchFieldException, IllegalAccessException {
-        TransactionIDIterator transactionIDIterator = new TransactionIDIterator();
+        TransactionIDIterator iterator = new TransactionIDIterator();
 
         Field current = TransactionIDIterator.class.getDeclaredField("current");
         current.setAccessible(true);
-        current.set(transactionIDIterator, 0xFFFF_FFFEL);
+        current.set(iterator, 0xFFFF_FFFEL);
 
-        assertThat(transactionIDIterator.next().longValue(), is(0xFFFF_FFFEL));
+        assertThat(iterator.next().longValue(), is(0xFFFF_FFFEL));
         // Skip 0xFFFF_FFFFL because that is reserved.
-        assertThat(transactionIDIterator.next().longValue(), is(0L));
+        assertThat(iterator.next().longValue(), is(0L));
     }
 }
