4f35c042041b638e6ca106223db686343093f1b4
Hideki Shiro <hideki@shiro.be>
2015-07-17 23:16:58 -0700

    Fix bug for TransactionIterator cyclic value.

--- lib/src/main/java/org/theta4j/ptp/TransactionIDIterator.java
+++ lib/src/main/java/org/theta4j/ptp/TransactionIDIterator.java
@@ -9,7 +9,7 @@ import org.theta4j.ptp.type.UINT32;
 import java.util.Iterator;
 
 /**
- * Iterator of Transaction ID for PTP-IP Packet
+ * Iterator of Transaction ID defined in PTP
  */
 public final class TransactionIDIterator implements Iterator<UINT32> {
     private long current = 0;
@@ -24,12 +24,13 @@ public final class TransactionIDIterator implements Iterator<UINT32> {
 
     /**
      * Returns the following values from initial state.
-     * 0, 1, 2, ..., 0xFFFFFFFE, 0, 1, 2, ...
+     * 0, 1, 2, ..., 0xFFFFFFFE, 1, 2, ...
      */
     @Override
     public UINT32 next() {
         if (0xFFFF_FFFFL <= current) {
-            current = 0;
+            // Initial value is 0 but cyclic initial value is 1
+            current = 1;
         }
 
         return new UINT32(current++);
