f82ffacf6f8c3969dea0837c3214a7715d49c364
Hideki Shiro <hideki@shiro.be>
2015-07-20 22:16:00 -0700

    Simplify ThetaTest

--- lib/src/integrationTest/java/org/theta4j/ThetaTest.java
+++ lib/src/integrationTest/java/org/theta4j/ThetaTest.java
@@ -4,26 +4,11 @@
 
 package org.theta4j;
 
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.IOException;
 
-public class ThetaTest {
-    private static Theta theta;
-
-    @BeforeClass
-    public static void connect() throws IOException {
-        theta = new Theta();
-    }
-
-    @AfterClass
-    public static void close() throws IOException, InterruptedException {
-        theta.close();
-        Thread.sleep(TestParameters.INTERVAL_AFTER_CLOSE);
-    }
-
+public class ThetaTest extends BaseThetaTest {
     @Test
     public void getDeviceInfo() throws IOException {
         theta.getDeviceInfo();
